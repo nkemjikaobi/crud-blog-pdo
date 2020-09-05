eb42e4d5dbbc8f8079529fe832e1cb06d7eccb68
nkemjikaobi <nkemjikaobi@gmail.com>
2020-09-05 19:37:58 +0100

    Htaccess and Modified DB

--- db.php
+++ db.php
@@ -5,11 +5,18 @@ $username = 'root';
 $password = '';
 $database  = 'blog-pdo';
 
+// $host = 'us-cdbr-east-02.cleardb.com';
+// $username = 'b697b23a2fead0';
+// $password = '6bf94e9c';
+// $database  = 'heroku_fcb8f6dcd1a86ce';
+
 
 $conn = new mysqli($host,$username, $password,$database);
 
 if(!$conn){
     die("Database Connection Error");
 }
-
+/*
+mysql://b697b23a2fead0:6bf94e9c@us-cdbr-east-02.cleardb.com/heroku_fcb8f6dcd1a86ce?reconnect=true
+*/
 ?>
