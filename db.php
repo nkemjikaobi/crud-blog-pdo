590206e39da2bb25d02a694f23620ad6b108db8a
Nkemjika Obi <nkemjikaobi@gmail.com>
2020-09-17 13:56:56 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database  = 'blog-pdo';
+// $host = 'localhost';
+// $username = 'root';
+// $password = '';
+// $database  = 'blog-pdo';
 
-// $host = 'us-cdbr-east-02.cleardb.com';
-// $username = 'b697b23a2fead0';
-// $password = '6bf94e9c';
-// $database  = 'heroku_fcb8f6dcd1a86ce';
+$host = 'us-cdbr-east-02.cleardb.com';
+$username = 'b697b23a2fead0';
+$password = '6bf94e9c';
+$database  = 'heroku_fcb8f6dcd1a86ce';
 
 
 $conn = new mysqli($host,$username, $password,$database);
@@ -17,15 +17,15 @@ if(!$conn){
     die("Database Connection Error");
 }
 
-// $sql = "CREATE TABLE users (
-//     id int(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-//     first_name varchar(255) NOT NULL,
-//     last_name varchar(255) NOT NULL,
-//     email varchar(255) NOT NULL,
-//     password varchar(255) NOT NULL,
-//     isAdmin tinyint(1) DEFAULT '0' NOT NULL,
-//     created_at timestamp DEFAULT CURRENT_TIMESTAMP
-// )";
+$sql = "CREATE TABLE users (
+    id int(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    isAdmin tinyint(1) DEFAULT '0' NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP
+)";
 // $sql = "CREATE TABLE blogs (
 //     id int(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 //     title varchar(255) NOT NULL,
